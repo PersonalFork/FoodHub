3021ca27df468d8d23c2626f88b504818e95f62d
Jashobantac <jashobantac@gmail.com>
2021-07-07 15:34:41 +0530

    Added solution and project files.

---
+++ FoodHub.Web.Main/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace FoodHub.Web.Main.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
